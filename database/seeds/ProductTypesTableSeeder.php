55f67d9713d55b3d97621462381bb85ff7b5475e
La Thai Binh <binhlt@saigonhitech.vn>
2018-08-16 09:20:56 +0700

    no message

---
+++ database/seeds/ProductTypesTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+class ProductTypesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        DB::table('product_types')->insert([
+            
+        ]);
+    }
+}
